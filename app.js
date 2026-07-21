d9df75c4b8306ed25de8835a044f1d3b922f7414
sanlith340-droid <sanlith340@gmail.com>
2026-07-21 11:16:00 -0500

    PUERTO URL PRUEBA ENDPOINT

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 import express from "express"; //ES
-import {configDotenv} from "dotenv"
-configDotenv()
+//import {configDotenv} from "dotenv"
+//configDotenv()
 //const express = require("express")//CJS
+import "dotenv/config"
+
 const app = express()
 const puerto = process.env.PORT || 3000;
 
 app.get("/",(req,res)=>{
-    res.send("Hola ficha 3407180 del SENA")
+    res.send("Hola  3407180 del SENA")
+})
+
+app.listen(puerto,()=>{
+    console.log(`Servidor funcionando en el puerto ${puerto}!!`);
+})
+
+
+
+app.get("/saludo",(req,res)=>{
+    res.send(`<h3> Hola Gente de YouTube soy el Loquendero340 y hoy vamos hablar de python XDXD </h3>`)
 })
 
 app.listen(puerto,()=>{
