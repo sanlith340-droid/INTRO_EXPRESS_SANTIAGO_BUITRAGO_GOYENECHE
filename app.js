7be586325c65b94352481a07d2c424e6ff40a907
sanlith340-droid <sanlith340@gmail.com>
2026-07-21 10:45:03 -0500

    FUNCIONES

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-const express = require("express")
-const express = require ("express")
+import express from "express"; //ES
+import {configDotenv} from "dotenv"
+configDotenv()
+//const express = require("express")//CJS
+const app = express()
+const puerto = process.env.PORT || 3000;
 
-//import express from express() //
+app.get("/",(req,res)=>{
+    res.send("Hola ficha 3407180 del SENA")
+})
 
-const express = express()
-const puerto= 3000
-
-
+app.listen(puerto,()=>{
+    console.log(`Servidor funcionando en el puerto ${puerto}!!`);
+})
